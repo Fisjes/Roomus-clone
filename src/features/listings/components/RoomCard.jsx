2961adf64f719e041cd042384a7cbb67d542b64e
Fisjes <leahvanderwesten@gmail.com>
2025-10-08 13:17:07 +0200

    replaced global css with tailwind for .btn, improved layout and pagination

--- src/features/listings/components/RoomCard.jsx
+++ src/features/listings/components/RoomCard.jsx
@@ -6,7 +6,7 @@ export default function RoomCard({ room }) {
       <div className="card__media" aria-hidden>
         <div className="imgph" />
       </div>
-      <div className="card__body">
+      <div className="card__body flex flex-col justify-between flex-1 p-3 text-left">
         <h3 className="card__title">{room.title}</h3>
         <p className="card__meta">
           <strong>€{room.price}</strong>/month · {room.neighborhood}, {room.city}
@@ -18,7 +18,7 @@ export default function RoomCard({ room }) {
           Amenities: {room.amenities.slice(0,3).join(', ')}
           {room.amenities.length > 3 ? '…' : ''}
         </p>
-        <Link className="btn" to={`/rooms/${room.id}`}>View details</Link>
+        <Link className="bg-black text-white p-2 rounded-xl text-center w-30 mt-2" to={`/rooms/${room.id}`}>View details</Link>
       </div>
     </div>
   );
